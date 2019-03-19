3927e37a96d8aef7a8192f63c247de126a626888
Nicholas Assaderaghi <nickassader@gmail.com>
2019-03-19 15:24:18 -0700

    reee

--- Commandable.java
+++ Commandable.java
@@ -1,11 +1,7 @@
-
-/**
- * Write a description of interface Commandable here.
- *
- * @author (your name)
- * @version (a version number or a date)
- */
+import java.util.*;
 public interface Commandable
 {
+    boolean checkCommand();
+    boolean checkTarget();
     public Result runCommand();
 }
--- Player.java
+++ Player.java
@@ -1,10 +1,3 @@
-
-/**
- * Write a description of class Player here.
- *
- * @author (your name)
- * @version (a version number or a date)
- */
 public class Player
 {
     public Player()
