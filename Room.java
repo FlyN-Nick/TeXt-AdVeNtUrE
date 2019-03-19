c869751b7e21debbb61d49eb3e659e83348120e4
Nicholas Assaderaghi <nickassader@gmail.com>
2019-03-19 15:25:38 -0700

    yeet

--- Room.java
+++ Room.java
@@ -4,6 +4,14 @@ public class Room implements Commandable
     public Room()
     {
     }
+    public boolean checkCommand()
+    {
+        return true;
+    }
+    public boolean checkTarget()
+    {
+        return true;
+    }
     public Result runCommand()
     {
         return new Result();
