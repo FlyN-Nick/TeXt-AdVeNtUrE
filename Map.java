c7fe0cb2135adf30547a0a32538c626cd0ca1e06
Alex <alexanderdfree@gmail.com>
2019-03-21 15:17:39 -0700

    this should only change map

--- Map.java
+++ Map.java
@@ -7,27 +7,10 @@
  */
 public class Map
 {
-    // instance variables - replace the example below with your own
-    private int x;
-
-    /**
-     * Constructor for objects of class Map
-     */
     public Map()
     {
         // initialise instance variables
-        x = 0;
+        String x = "Hotel, Motel, Brotel, Shmotel, Otel, Tel, El, and L.";
     }
 
-    /**
-     * An example of a method - replace this comment with your own
-     *
-     * @param  y  a sample parameter for a method
-     * @return    the sum of x and y
-     */
-    public int sampleMethod(int y)
-    {
-        // put your code here
-        return x + y;
-    }
 }
