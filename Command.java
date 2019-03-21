2eb18e9f47e0a1f3d9b5966f955c823d032d0056
Nicholas Assaderaghi <nickassader@gmail.com>
2019-03-21 14:56:38 -0700

    yeet

--- Command.java
+++ Command.java
@@ -1,6 +1,7 @@
 import java.util.*;
 public class Command
 {
+    String target;
     public Command()
     {
     }
--- Parser.java
+++ Parser.java
@@ -1,11 +1,21 @@
 import java.util.*;
 public class Parser
 {
+    static public ArrayList<String> allPossibleCommands = new ArrayList<String>();
+    static public ArrayList<String> unlockedCommands = new ArrayList<String>();
     public Parser()
     {
+        if (allPossibleCommands == null)
+        { // make the commands
+            //allPossibleCommands.add("");
+        }
     }
     public Command parseInput(String input)
     {
         return new Command();
+    }
+    public void addUnlockedCommand(String commandName)
+    {
+        unlockedCommands.add(commandName);
     }
 }
