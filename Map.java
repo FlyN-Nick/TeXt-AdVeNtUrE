b6e659efe4da2cf198d41bbd0f1718d3e09c7069
davfrei <48737072+davfrei@users.noreply.github.com>
2019-03-26 15:17:06 -0700

    Update Map.java

--- Map.java
+++ Map.java
@@ -1,8 +1,16 @@
+/**
+ * Write a description of class Map here.
+ *
+ * @author (your name)
+ * @version (a version number or a date)
+ */
 public class Map
 {
-    public Map()
-    {
-        // initialise instance variables
-        String x = "Hotel, Motel, Brotel, Shmotel, Otel, Tel, El, and L.";
+    Room currentRoom(){ 
+        return null;
+    }
+    
+    Room moveToRoom( Room nextRoom ) {
+        return currentRoom();
     }
 }
