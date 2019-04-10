0c54d394bb000d2a4c0815a2316cc6bff907447e
Alex <alexanderdfree@gmail.com>
2019-04-10 11:15:02 -0700

    made boolean on item work for commancheck

--- Item.java
+++ Item.java
@@ -9,18 +9,20 @@ public class Item
         this.itemName = itemName;
     }
     boolean checkAvailability(){
+        //function not done yet
         return true;
     }
     boolean checkCommand(String command)
     {
+        boolean returnval = false;
         if (acceptableCommands.contains(command))
         {
-            return true;
+            returnval = true;
         }
-        return false;
+        return returnval;
     }
     public Result runCommand(String command)
     {
-        return new Result();//commandsToResult.get(command)
+        return new Result();//commandsToResult.get(command) <- that code not working so i changed it to result
     }
 }
