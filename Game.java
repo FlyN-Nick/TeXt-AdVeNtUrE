d67c184b5cfaad5ea79f6e7cc21e7d916f82adb4
David <davfrei@nuevaschool.org>
2019-04-11 15:10:12 -0700

    d

--- Game.java
+++ Game.java
@@ -3,6 +3,13 @@ public class Game
     private Map map;
     private Player player;
     private Parser parser;
+    
+    public void startGame(){
+        int i=0;
+        while (i>-1){
+            
+        }
+    }
 }
 
 
