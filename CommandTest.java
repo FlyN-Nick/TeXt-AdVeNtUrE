c60d647b31ac5773f39a778e3d94a95294c2acc4
Alex <alexanderdfree@gmail.com>
2019-04-25 14:45:07 -0700

    test

--- CommandTest.java
+++ CommandTest.java
@@ -7,27 +7,9 @@
  */
 public class CommandTest
 {
-    // instance variables - replace the example below with your own
+
     private int x;
-
-    /**
-     * Constructor for objects of class CommandTest
-     */
-    public CommandTest()
-    {
-        // initialise instance variables
-        x = 0;
-    }
-
-    /**
-     * An example of a method - replace this comment with your own
-     *
-     * @param  y  a sample parameter for a method
-     * @return    the sum of x and y
-     */
-    public int sampleMethod(int y)
-    {
-        // put your code here
-        return x + y;
+    private boolean commandTester();
+    
     }
 }
