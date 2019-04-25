4ffea23ea607f87647209f0dc2710ff6e9c85f25
Alex <alexanderdfree@gmail.com>
2019-04-25 14:59:55 -0700

    added some stuff and changed thingys

---
+++ CommandTestable.java
@@ -0,0 +1,7 @@
+public interface CommandTestable
+{
+    public static boolean commandTester(){
+        return false;
+    }
+    public void commandester();
+}
--- Game.java
+++ Game.java
@@ -3,9 +3,9 @@ public class Game
     private Map map;
     private Player player;
     private Parser parser;
-
+    public CommandTestable testerInterface;
     private Game rsRiseUp;
-    
+    public Commandable tester;
     public Map startGamersWhoAreRisingUp(){
            //this is a test to see if the class is functional and the map object
            return map;
