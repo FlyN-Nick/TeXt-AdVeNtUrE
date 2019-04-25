62f7a338ec473df4bdf51a3dadc3ba7395f1575e
Alex <alexanderdfree@gmail.com>
2019-04-25 14:24:29 -0700

    gmare

--- Game.java
+++ Game.java
@@ -3,7 +3,14 @@ public class Game
     private Map map;
     private Player player;
     private Parser parser;
-    private Game We_are_GAMERZ;
+    private Game rsRiseUp;
+    
+    public void startGamersWhoAreRisingUp(){
+        int i=0;
+        while (i>-1){
+            
+        }
+    }
 }
 
 
